c3855121fd7e76e0a77585853dea32d92f3ba88d
Md. Injamamul Haque <i.haq.bd@gmail.com>
2025-03-21 16:51:01 +0600

    Create initialize_database_and_schemas.sql

---
+++ sql_scripts/initialize_database_and_schemas.sql
@@ -0,0 +1,28 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse'. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+*/
+
+USE master;
+GO
+
+-- creating database named 'DataWarehouse'
+CREATE database DataWarehouse;
+
+USE DataWarehouse;
+GO
+
+-- Creating schemas: bronze, silver, gold
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
